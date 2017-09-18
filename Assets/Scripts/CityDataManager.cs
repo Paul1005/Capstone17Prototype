d1c93f7abf4adec658d6519f420708aa6dc0c5d1
Shawn <sdf1342@gmail.com>
2017-09-18 16:08:57 -0700

    #starts grid creation

---
+++ Assets/Scripts/CityDataManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityDataManager : MonoBehaviour {
+
+	public int population = 1000;
+	public int size_x = 10;
+	public int size_y = 10;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public int getSizeX (){
+		return size_x;
+	}
+
+	public int getSizeY (){
+		return size_y;
+	}
+
+	public int getPopulation (){
+		return population;
+	}
+
+}
