b1b474925e9be23fe479d0b96aaa223239e9d7fc
Shawn <sdf1342@gmail.com>
2017-09-19 00:01:41 -0700

    - Unity version Upgrade from 5.6.2 to 2017.1.1f1 - Grid Creation, GridColor scripts created and modified (working version)

---
+++ Assets/GridColor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * GridColor that color sprite renderer to represent zone
+ * 
+ * Author: 		Dongwon(Shawn) Kim
+ * Date(Cr): 	2017-09-18
+ * Date(Mo):	2017-09-18 
+ * Reference:
+ * https://gamedev.stackexchange.com/questions/134000/unity-2d-coordinates
+ **/
+public class GridColor : MonoBehaviour {
+	private Color[] Colors = new Color[4] { Color.red, Color.blue, Color.green, Color.yellow };
+	public int index;
+
+	void Awake()
+	{
+		index = Random.Range(0, Colors.Length);
+		var spriteRenderer = GetComponent<SpriteRenderer>();
+		spriteRenderer.color = Colors[index];
+	}
+
+}
---
+++ Assets/GridCreation.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * GridCreation that creates 3D based on data from CityDataManager
+ * 
+ * Author: 		Dongwon(Shawn) Kim
+ * Date(Cr): 	2017-09-18
+ * Date(Mo):	2017-09-18 
+ * Reference:	
+ * http://answers.unity3d.com/questions/718778/trying-to-create-a-grid.html
+ * https://docs.unity3d.com/Manual/InstantiatingPrefabs.html
+ * http://answers.unity3d.com/questions/25352/add-texture-to-gameobject-through-code.html
+ * https://www.youtube.com/watch?v=810UVUWGlWw
+ **/
+public class GridCreation : MonoBehaviour {
+
+	public Transform CellPrefab;
+	public Vector3 Size;
+	public int GridWidth;
+	public int GridHeight;
+
+	// Use this for initialization
+	void Start () {
+		CreateGrid ();
+	}
+
+	void CreateGrid(){
+//		GameObject text = new GameObject();
+//		TextMesh t = text.AddComponent<TextMesh>();
+//		t.text = "1";
+//		t.fontSize = 30;
+
+		Debug.Log (CellPrefab.localScale.x +", " + CellPrefab.localScale.z);
+
+		for(int x = 0; x < Size.x; x++){
+			for(int z = 0; z < Size.z; z++){
+				Instantiate(CellPrefab, new Vector3(x + CellPrefab.localScale.x*x,
+													0,
+													z+CellPrefab.localScale.z*z),
+													Quaternion.identity);
+			}
+		}
+	}
+}
